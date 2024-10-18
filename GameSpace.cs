c1a212e05982152456775f1a4a11c55ddccd35c1
InteractMe 3 <114359126+1nteractme@users.noreply.github.com>
2024-10-19 01:36:58 +0300

    + GameSpace класс, холдящий объекты игрового пространства для отключения их видимости

---
+++ GameSpace.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameSpace : MonoBehaviour
+{
+    public TMP_Text Inform;
+    public GameObject Board;
+    public Button Restart, Back;
+}
